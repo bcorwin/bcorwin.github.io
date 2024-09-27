652a22deae493c54519bed56e3ff5a2cfc7587a6
ben <bscorwin@gmail.com>
2024-09-27 12:50:49 -0500

    [#10] [#9] refactor email and add resume download, code clean-up

--- assets/js/myscripts.js
+++ assets/js/myscripts.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   const links = document.querySelectorAll('a')
   links.forEach(link => {
-    if (link.href.includes('http')) {
+    if (link.href.includes('http') | link.href.includes('mailto:')) {
       link.setAttribute('target', '_blank')
     }
   })
 })
+
+function downloadResume() {
+  var fileId = "1NmyAmL2zDTJAobymOxh_Jk5xR3PiNyZt"
+  var downloadLink = `https://drive.google.com/uc?export=download&id=${fileId}`;
+  window.open(downloadLink, '_blank');
+}
+  
