005009646b90e706207d443e79120a7e7b5eeca2
ben <bscorwin@gmail.com>
2024-09-27 11:38:12 -0500

    [#7] open all external links in new tab and fix asset paths

---
+++ assets/js/myscripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('a')
+  links.forEach(link => {
+    if (link.href.includes('http')) {
+      link.setAttribute('target', '_blank')
+    }
+  })
+})
